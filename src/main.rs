634a886f08d8c978fb032e55ccc2917a1248af95
SuperJaremy <legoitcomp@gmail.com>
2025-07-17 21:54:25 +0200

    test csv deserialization
    code for `main.rs` is taken from `csv` crate's tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// Copied from csv crate tutorial
+use std::{env, error::Error, ffi::OsString, process};
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_frist_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn get_frist_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 arguemnt, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
