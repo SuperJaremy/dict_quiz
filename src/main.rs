0204bd0e90a2cb9bc6e0e153cb74892d72e0cda0
SuperJaremy <legoitcomp@gmail.com>
2025-07-18 20:58:06 +0200

    add base loop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
-// Copied from csv crate tutorial
-use std::{env, error::Error, ffi::OsString, process};
+use serde::Deserialize;
+use std::{env, error::Error, ffi::OsString, io, process};
+
+#[derive(Debug, Deserialize)]
+struct Word {
+    #[serde(rename = "word")]
+    word: String,
+    #[serde(rename = "translation")]
+    translation: String,
+    #[serde(rename = "class")]
+    class: String,
+    #[serde(rename = "definite(noun)")]
+    definite: Option<String>,
+    #[serde(rename = "plural(noun;adj)")]
+    plural: Option<String>,
+    #[serde(rename = "neuter(adj)")]
+    neuter: Option<String>,
+    #[serde(rename = "present(verb)")]
+    present: Option<String>,
+    #[serde(rename = "past(verb)")]
+    past: Option<String>,
+    #[serde(rename = "possessive(pronoun)")]
+    possesive: Option<String>,
+}
 
 fn run() -> Result<(), Box<dyn Error>> {
     let file_path = get_frist_arg()?;
     let mut rdr = csv::Reader::from_path(file_path)?;
 
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+    for result in rdr.deserialize() {
+        let record: Word = result?;
+        if ask_translation(&record) {
+            println!("Correct!");
+        } else {
+            println!("Incorrect");
+        }
     }
 
     Ok(())
@@ -18,6 +44,25 @@ fn get_frist_arg() -> Result<OsString, Box<dyn Error>> {
         None => Err(From::from("expected 1 arguemnt, but got none")),
         Some(file_path) => Ok(file_path),
     }
+}
+
+fn ask_translation(word: &Word) -> bool {
+    println!("{}", word.word);
+    println!("Translate this word in English");
+
+    let mut answer = String::new();
+
+    loop {
+        match io::stdin().read_line(&mut answer) {
+            Ok(_) => break,
+            Err(_) => {
+                println!("Try again");
+                answer.clear();
+            }
+        }
+    }
+
+    word.translation == answer.trim()
 }
 
 fn main() {
