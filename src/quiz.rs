ce38e7e60f186a6d3cc6458a362f2db40fb48451
SuperJaremy <legoitcomp@gmail.com>
2026-07-16 08:02:32 +0200

    Use &dyn instead of &Box<dyn>
    - plus LSP

--- src/quiz.rs
+++ src/quiz.rs
@@ -1,12 +1,12 @@
 use std::error::Error;
 use std::rc::Rc;
 
-use rand::prelude::IndexedRandom;
-use crate::cmp;
 use crate::Pick;
-use view::View;
-use crate::{word::question::{Question, categories::Category}};
+use crate::cmp;
+use crate::word::question::{Question, categories::Category};
+use rand::prelude::IndexedRandom;
 use rand::{SeedableRng, rngs::SmallRng};
+use view::View;
 
 pub mod console;
 pub mod view;
@@ -35,37 +35,45 @@ impl QuizConfig {
 
 /// Quiz's end result to display
 pub struct QuizResults<'a> {
-	// quiz: &'a Quiz<'a>,
+    // quiz: &'a Quiz<'a>,
     quiz: Quiz<'a>,
     correct_num: u32,
-    wrong_answers: Vec<&'a Box<dyn Pick>>,
+    wrong_answers: Vec<&'a dyn Pick>,
 }
 
 struct QuizData<'a> {
-	questions: Vec<(&'a Box<dyn Pick>, Question)>,
-	config: QuizConfig,
-	view: &'a dyn View,
+    questions: Vec<(&'a dyn Pick, Question)>,
+    config: QuizConfig,
+    view: &'a dyn View,
 }
 
-pub struct Quiz<'a> (Rc<QuizData<'a>>);
+pub struct Quiz<'a>(Rc<QuizData<'a>>);
 
 impl<'a> Quiz<'_> {
-	pub fn new(mut config: QuizConfig, dict: &'a [Box<dyn Pick>], view: &'a dyn View) -> Quiz<'a> {
-		let questions_num = cmp::min(dict.len(), config.question_num);
-		config.question_num = questions_num;
+    pub fn new(mut config: QuizConfig, dict: &'a [Box<dyn Pick>], view: &'a dyn View) -> Quiz<'a> {
+        let questions_num = cmp::min(dict.len(), config.question_num);
+        config.question_num = questions_num;
 
-		let mut rng = SmallRng::from_os_rng();
-		let questions = dict[..]
+        let mut rng = SmallRng::from_os_rng();
+        let questions = dict[..]
             .choose_multiple(&mut rng, questions_num)
-            .map(|word| (word, word.get_question(&config.category))).collect();
-        Quiz(Rc::new(QuizData { questions, config, view}))
-	}
+            .map(|word| {
+                let w = word.as_ref();
+                (w, w.get_question(&config.category))
+            })
+            .collect();
+        Quiz(Rc::new(QuizData {
+            questions,
+            config,
+            view,
+        }))
+    }
 
-	pub fn start(self) -> Result<(), Box<dyn Error>> {
-		let mut wrongs = Vec::new();
+    pub fn start(self) -> Result<(), Box<dyn Error>> {
+        let mut wrongs = Vec::new();
         let mut correct: u32 = 0;
 
-		for (w, q) in &self.0.questions {
+        for (w, q) in &self.0.questions {
             if self.0.view.ask_question(q)? {
                 correct += 1;
             } else {
@@ -82,5 +90,5 @@ impl<'a> Quiz<'_> {
         self.0.view.display_results(&result)?;
 
         Ok(())
-	}
+    }
 }
